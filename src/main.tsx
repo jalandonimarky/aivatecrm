c19cd3fa9802cbbbd51aff026740c9840e0dbf61
Mark Anthony Jalandoni <markyjalandoni.mj@gmail.com>
2025-07-19 12:59:05 +0800

    [dyad] Corrected the default theme to light, enabling proper theme toggling. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { ThemeProvider } from './components/theme/ThemeProvider.tsx'
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+  <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
     <App />
   </ThemeProvider>
 );
