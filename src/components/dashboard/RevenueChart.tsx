d4ac48a8577113499853c4bc9c208342d6465ece
Mark Anthony Jalandoni <markyjalandoni.mj@gmail.com>
2025-07-25 17:18:05 +0800

    [dyad] Reverted non-header text colors to default - wrote 6 file(s) + extra files edited outside of Dyad

--- src/components/dashboard/RevenueChart.tsx
+++ src/components/dashboard/RevenueChart.tsx
@@ -36,7 +36,7 @@ export function RevenueChart({ paidDeals, cancelledDeals, pipelineValue }: Reven
   return (
     <Card className="bg-gradient-card border-border/50">
       <CardHeader>
-        <CardTitle className="text-lg font-semibold text-accent">
+        <CardTitle className="text-lg font-semibold">
           Revenue Overview
         </CardTitle>
       </CardHeader>
