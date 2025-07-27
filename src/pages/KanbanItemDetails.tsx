7e7004ef3af1a7063532388fa3c0d283b0e42ada
Mark Anthony Jalandoni <markyjalandoni.mj@gmail.com>
2025-07-27 21:03:34 +0800

    [dyad] Fix missing Textarea import - wrote 1 file(s)

--- src/pages/KanbanItemDetails.tsx
+++ src/pages/KanbanItemDetails.tsx
@@ -13,6 +13,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
